9781c82050eb4b53d44e5c0bdaa859025b9a99f9
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-17 15:21:54 +0200

    feat: add RestClient and Security configuration with CORS support

---
+++ server/src/main/java/com/djmahirnationtv/status/backend/config/RestClientConfig.java
@@ -0,0 +1,23 @@
+package com.djmahirnationtv.status.backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+    @Bean
+    public RestClient pingResetClient() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(5));
+        requestFactory.setReadTimeout(Duration.ofSeconds(10));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
+    }
+}
