b2d420b0fc70147c239099b0198789d9e331502a
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-09-22 17:19:52 +0200

    fix: database integration test

--- server/src/test/java/com/djmahirnationtv/status/backend/BackendApplicationTests.java
+++ server/src/test/java/com/djmahirnationtv/status/backend/BackendApplicationTests.java
@@ -1,16 +1,9 @@
 package com.djmahirnationtv.status.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-	DataSourceAutoConfiguration.class,
-	MongoAutoConfiguration.class
-})
 class BackendApplicationTests {
 
 	@Test
